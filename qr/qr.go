abef830a5cd99eea7bb49aedca9c9f2e6ed52fb6
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-04-27 15:22:58 +0530

    CHANGE QRCODE SIZE

--- qr/qr.go
+++ qr/qr.go
@@ -9,11 +9,14 @@ import (
 // RenderString as a QR code
 func RenderString(s string) {
 	qrConfig := qrterminal.Config{
-		Level:     qrterminal.L,
-		Writer:    os.Stdout,
-		BlackChar: qrterminal.WHITE,
-		WhiteChar: qrterminal.BLACK,
-		QuietZone: 2,
+		HalfBlocks:     true,
+		Level:          qrterminal.L,
+		Writer:         os.Stdout,
+		BlackWhiteChar: "\u001b[37m\u001b[40m\u2584\u001b[0m",
+		BlackChar:      "\u001b[30m\u001b[40m\u2588\u001b[0m",
+		WhiteBlackChar: "\u001b[30m\u001b[47m\u2585\u001b[0m",
+		WhiteChar:      "\u001b[37m\u001b[47m\u2588\u001b[0m",
+		QuietZone:      1,
 	}
 	qrterminal.GenerateWithConfig(s, qrConfig)
 }
