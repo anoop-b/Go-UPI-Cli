b01e3e3397bb1981ea5c300cdc0cd8fc4f12fe4d
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-05-31 23:59:03 +0530

    specify exact arg count for qrcode

--- cmd/qrcode.go
+++ cmd/qrcode.go
@@ -38,6 +38,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("qrcode generate for:", args[0])
 		qr.RenderString(args[0])
