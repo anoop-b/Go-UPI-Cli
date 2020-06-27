c55a28bc72c10e4921acfdd2cff3fd7b18ef0540
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-06-28 00:22:00 +0530

    add docs command

---
+++ docs/repo.go
@@ -0,0 +1,32 @@
+package docs
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+const (
+	docsURL = "https://github.com/anoop-b/"
+)
+
+//Launch url for project wiki/documentation
+func Launch() error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", docsURL).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", docsURL).Start()
+	case "darwin":
+		err = exec.Command("open", docsURL).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		return fmt.Errorf("open docs: %w", err)
+	}
+
+	return err
+}
